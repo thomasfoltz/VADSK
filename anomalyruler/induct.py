f9b7324eefdf68af41f0a34d4ff81517a2ce851f
thomasfoltz <thomasfoltz42@gmail.com>
2024-11-10 14:54:40 -0500

    induction on UCSDped2 dataset

--- anomalyruler/induct.py
+++ anomalyruler/induct.py
@@ -68,7 +68,6 @@ if __name__ == "__main__":
     args = parse_arguments()
     batch, batch_size = args.b, args.bs
     data_name = args.data
-    dataset_name = {'SHTech':'ShanghaiTech', 'avenue':'CUHK Avenue', 'ped2': 'UCSD Ped2', 'UBNormal': 'UBNormal'}[args.data]
     
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
